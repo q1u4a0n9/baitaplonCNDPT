316aa105b6ca31b76128b066a25a3a324870cc12
Nam Dang <namdp.28tech@gmail.com>
2025-03-01 20:37:27 +0700

    2.4. Tách cấu hình database

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -3,7 +3,7 @@ const Tour = require("../../models/tour.model");
 module.exports.list = async (req, res) => {
   const tourList = await Tour.find({});
 
-  console.log(tourList);
+  // console.log(tourList);
 
   res.render("client/pages/tour-list", {
     pageTitle: "Danh sách tour",
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE);
-
+const database = require("./config/database");
 const clientRoutes = require("./routes/client/index.route");
 
 const app = express()
 const port = 3000
+
+// Kết nối database
+database.connect();
 
 // Thiết lập thư mục views và view engine pug
 app.set('views', path.join(__dirname, 'views')); // Thư mục chứa file Pug
