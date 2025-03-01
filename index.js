92eda97cfb617709f4d742d54da267856207808b
Nam Dang <namdp.28tech@gmail.com>
2025-03-01 19:44:06 +0700

    10. Kết nối Project 5 với cơ sở dữ liệu

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE);
+
+const Tour = mongoose.model('Tour', {
+  name: String,
+  vehicle: String
+});
+
 const app = express()
 const port = 3000
 
@@ -16,9 +25,14 @@ app.get('/', (req, res) => {
   });
 })
 
-app.get('/tours', (req, res) => {
+app.get('/tours', async (req, res) => {
+  const tourList = await Tour.find({});
+
+  console.log(tourList);
+
   res.render("client/pages/tour-list", {
-    pageTitle: "Danh sách tour"
+    pageTitle: "Danh sách tour",
+    tourList: tourList
   });
 })
 
