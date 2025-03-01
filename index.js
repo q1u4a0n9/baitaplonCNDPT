a643742f0986dd29ab3a41b507ae3f18127323dc
Nam Dang <namdp.28tech@gmail.com>
2025-03-01 20:14:42 +0700

    2.2. Tách Controller

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE);
 
-const Tour = require("./models/tour.model");
+const homeController = require("./controllers/client/home.controller");
+const tourController = require("./controllers/client/tour.controller");
 
 const app = express()
 const port = 3000
@@ -16,22 +17,9 @@ app.set('view engine', 'pug'); // Thiết lập pug làm view engine
 // Thiết lập thư mục chứ file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render("client/pages/home", {
-    pageTitle: "Trang chủ"
-  });
-})
+app.get('/', homeController.home)
 
-app.get('/tours', async (req, res) => {
-  const tourList = await Tour.find({});
-
-  console.log(tourList);
-
-  res.render("client/pages/tour-list", {
-    pageTitle: "Danh sách tour",
-    tourList: tourList
-  });
-})
+app.get('/tours', tourController.list)
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
