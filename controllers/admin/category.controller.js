9446a4cc5ff827641b759054bcb894a77d795c4a
Nam Dang <namdp.28tech@gmail.com>
2025-03-04 21:00:53 +0700

    1.6. Tích hợp giao diện trang Tạo danh mục

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -3,3 +3,9 @@ module.exports.list = async (req, res) => {
     pageTitle: "Quản lý danh mục"
   });
 }
+
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/category-create", {
+    pageTitle: "Tạo danh mục"
+  });
+}
