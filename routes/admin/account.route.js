2ff41af5f9220ddffe308a2a9247ee142f942245
Nam Dang <namdp.28tech@gmail.com>
2025-03-11 20:55:19 +0700

    05. Tính năng Quên mật khẩu

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -14,6 +14,8 @@ router.get('/register-initial', accountController.registerInitial)
 
 router.get('/forgot-password', accountController.forgotPassword)
 
+router.post('/forgot-password', accountController.forgotPasswordPost)
+
 router.get('/otp-password', accountController.otpPassword)
 
 router.get('/reset-password', accountController.resetPassword)
