1502bea002f9a2ea6e6636df004cb9ad817520ad
Nam Dang <namdp.28tech@gmail.com>
2025-03-13 19:51:13 +0700

    03. Hiển thị thông tin tài khoản trên Header

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -26,6 +26,9 @@ module.exports.verifyToken = async (req, res, next) => {
     }
 
     req.account = existAccount;
+    res.locals.account = {
+      fullName: existAccount.fullName
+    };
 
     next();
   } catch (error) {
