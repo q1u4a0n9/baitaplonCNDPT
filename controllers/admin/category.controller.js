79b4ee1a346758ce8824f4a92387a80654ec3ecb
Nam Dang <namdp.28tech@gmail.com>
2025-03-20 20:06:56 +0700

    03. Làm trang Sửa danh mục

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -74,3 +74,66 @@ module.exports.createPost = async (req, res) => {
     // message: "Tạo danh mục thành công!"
   });
 }
+
+module.exports.edit = async (req, res) => {
+  try {
+    const categoryList = await Category.find({
+      deleted: false
+    });
+  
+    const categoryTree = categoryHelper.buildCategoryTree(categoryList, "");
+  
+    const id = req.params.id;
+    const categoryDetail = await Category.findOne({
+      _id: id,
+      deleted: false
+    });
+  
+    res.render("admin/pages/category-edit", {
+      pageTitle: "Chỉnh sửa danh mục",
+      categoryList: categoryTree,
+      categoryDetail: categoryDetail
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/${pathAdmin}/category/list`);
+  }
+}
+
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if(req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const totalRecord = await Category.countDocuments({});
+      req.body.position = totalRecord + 1;
+    }
+
+    req.body.updatedBy = req.account.id;
+
+    if(req.file) {
+      req.body.avatar = req.file.path;
+    } else {
+      delete req.body.avatar;
+    }
+
+    await Category.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body);
+
+    req.flash('success', 'Cập nhật danh mục thành công!');
+
+    res.json({
+      code: "success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Id không tồn tại trong hệ thống!"
+    })
+  }
+}
--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -14,4 +14,8 @@ router.get('/create', categoryController.create)
 
 router.post('/create', upload.single('avatar'), categoryValidate.createPost, categoryController.createPost)
 
+router.get('/edit/:id', categoryController.edit)
+
+router.patch('/edit/:id', upload.single('avatar'), categoryValidate.createPost, categoryController.editPatch)
+
 module.exports = router;
