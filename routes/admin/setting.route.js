26e3abae857020c281379cbd01d28114506a8204
Nam Dang <namdp.28tech@gmail.com>
2025-03-27 20:54:03 +0700

    02. Làm trang Tạo nhóm quyền

--- routes/admin/setting.route.js
+++ routes/admin/setting.route.js
@@ -33,4 +33,6 @@ router.get('/role/list', settingController.roleList)
 
 router.get('/role/create', settingController.roleCreate)
 
+router.post('/role/create', settingController.roleCreatePost)
+
 module.exports = router;
