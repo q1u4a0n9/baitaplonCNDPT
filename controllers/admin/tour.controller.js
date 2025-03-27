ee9391836ef7b96634f342bc780feb4a68eb7098
Nam Dang <namdp.28tech@gmail.com>
2025-03-27 19:32:50 +0700

    02. Làm trang Chỉnh sửa tour

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -97,3 +97,81 @@ module.exports.trash = async (req, res) => {
     pageTitle: "Thùng rác tour"
   });
 }
+
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const tourDetail = await Tour.findOne({
+      _id: id,
+      deleted: false
+    });
+
+    tourDetail.departureDateFormat = moment(tourDetail.departureDate).format('YYYY-MM-DD');
+
+    const categoryList = await Category.find({
+      deleted: false
+    });
+
+    const categoryTree = categoryHelper.buildCategoryTree(categoryList, "");
+
+    const cityList = await City.find({});
+
+    res.render("admin/pages/tour-edit", {
+      pageTitle: "Chỉnh sửa tour",
+      categoryList: categoryTree,
+      cityList: cityList,
+      tourDetail: tourDetail
+    });
+  } catch (error) {
+    res.redirect(`/${pathAdmin}/tour/list`);
+  }
+}
+
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if(req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const totalRecord = await Tour.countDocuments({});
+      req.body.position = totalRecord + 1;
+    }
+
+    req.body.updatedBy = req.account.id;
+    if(req.file) {
+      req.body.avatar = req.file.path;
+    } else {
+      delete req.body.avatar;
+    }
+    req.body.priceAdult = req.body.priceAdult ? parseInt(req.body.priceAdult) : 0;
+    req.body.priceChildren = req.body.priceChildren ? parseInt(req.body.priceChildren) : 0;
+    req.body.priceBaby = req.body.priceBaby ? parseInt(req.body.priceBaby) : 0;
+    req.body.priceNewAdult = req.body.priceNewAdult ? parseInt(req.body.priceNewAdult) : 0;
+    req.body.priceNewChildren = req.body.priceNewChildren ? parseInt(req.body.priceNewChildren) : 0;
+    req.body.priceNewBaby = req.body.priceNewBaby ? parseInt(req.body.priceNewBaby) : 0;
+    req.body.stockAdult = req.body.stockAdult ? parseInt(req.body.stockAdult) : 0;
+    req.body.stockChildren = req.body.stockChildren ? parseInt(req.body.stockChildren) : 0;
+    req.body.stockBaby = req.body.stockBaby ? parseInt(req.body.stockBaby) : 0;
+    req.body.locations = req.body.locations ? JSON.parse(req.body.locations) : [];
+    req.body.departureDate = req.body.departureDate ? new Date(req.body.departureDate) : null;
+    req.body.schedules = req.body.schedules ? JSON.parse(req.body.schedules) : [];
+
+    await Tour.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body);
+
+    req.flash("success", "Cập nhật tour thành công!");
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: error
+    })
+  }
+}
--- routes/admin/tour.route.js
+++ routes/admin/tour.route.js
@@ -15,4 +15,8 @@ router.post('/create', upload.single('avatar'), tourValidate.createPost, tourCon
 
 router.get('/trash', tourController.trash)
 
+router.get('/edit/:id', tourController.edit)
+
+router.patch('/edit/:id', upload.single('avatar'), tourValidate.createPost, tourController.editPatch)
+
 module.exports = router;
