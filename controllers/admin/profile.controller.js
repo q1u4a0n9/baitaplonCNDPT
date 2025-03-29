140aec63fe0149448a363af057d9375886d120da
Nam Dang <namdp.28tech@gmail.com>
2025-03-29 20:38:08 +0700

    05. Tính năng Đổi mặt khẩu tài khoản cá nhân

--- controllers/admin/profile.controller.js
+++ controllers/admin/profile.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const AccountAdmin = require("../../models/account-admin.model");
 const Role = require("../../models/role.model");
 
@@ -53,4 +54,32 @@ module.exports.changePassword = async (req, res) => {
   res.render("admin/pages/profile-change-password", {
     pageTitle: "Đổi mật khẩu"
   });
+}
+
+module.exports.changePasswordPatch = async (req, res) => {
+  try {
+    const id = req.account.id;
+
+    req.body.updatedBy = req.account.id;
+
+    // Mã hóa mật khẩu với bcrypt
+    const salt = await bcrypt.genSalt(10); // Tạo salt - Chuỗi ngẫu nhiên có 10 ký tự
+    req.body.password = await bcrypt.hash(req.body.password, salt); // Mã hóa mật khẩu
+
+    await AccountAdmin.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body);
+
+    req.flash('success', 'Đổi mật khẩu thành công!');
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: error
+    });
+  }
 }
