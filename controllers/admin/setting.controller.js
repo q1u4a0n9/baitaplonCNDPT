68e0e1fa9ac9ee0aa743478b973d5801f0f2ad9f
Nam Dang <namdp.28tech@gmail.com>
2025-03-29 19:05:58 +0700

    03. Làm trang Danh sách nhóm quyền

--- controllers/admin/setting.controller.js
+++ controllers/admin/setting.controller.js
@@ -66,8 +66,13 @@ module.exports.accountAdminCreate = async (req, res) => {
 }
 
 module.exports.roleList = async (req, res) => {
+  const roleList = await Role.find({
+    deleted: false
+  });
+
   res.render("admin/pages/setting-role-list", {
-    pageTitle: "Nhóm quyền"
+    pageTitle: "Nhóm quyền",
+    roleList: roleList
   });
 }
 
