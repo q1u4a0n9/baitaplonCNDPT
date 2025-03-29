5e2e1ba1ac8e23d558e6f37a272faff64c17e037
Nam Dang <namdp.28tech@gmail.com>
2025-03-29 20:10:57 +0700

    03. Làm trang Sửa tài khoản quản trị

--- controllers/admin/setting.controller.js
+++ controllers/admin/setting.controller.js
@@ -123,6 +123,60 @@ module.exports.accountAdminCreatePost = async (req, res) => {
   });
 }
 
+module.exports.accountAdminEdit = async (req, res) => {
+  try {
+    const roleList = await Role.find({
+      deleted: false
+    });
+
+    const id = req.params.id;
+    const accountAdminDetail = await AccountAdmin.findOne({
+      _id: id,
+      deleted: false
+    });
+  
+    res.render("admin/pages/setting-account-admin-edit", {
+      pageTitle: "Chỉnh sửa tài khoản quản trị",
+      roleList: roleList,
+      accountAdminDetail: accountAdminDetail
+    });
+  } catch (error) {
+    res.redirect(`/${pathAdmin}/setting/account-admin/list`);
+  }
+}
+
+module.exports.accountAdminEditPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    req.body.updatedBy = req.account.id;
+    if(req.file) {
+      req.body.avatar = req.file.path;
+    } else {
+      delete req.body.avatar;
+    }
+    
+    // Mã hóa mật khẩu với bcrypt
+    if(req.body.password) {
+      const salt = await bcrypt.genSalt(10); // Tạo salt - Chuỗi ngẫu nhiên có 10 ký tự
+      req.body.password = await bcrypt.hash(req.body.password, salt); // Mã hóa mật khẩu
+    }
+
+    await AccountAdmin.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body);
+
+    req.flash('success', 'Cập nhật tài khoản quản trị thành công!');
+
+    res.json({
+      code: "success"
+    });
+  } catch (error) {
+    res.redirect(`/${pathAdmin}/setting/account-admin/list`);
+  }
+}
+
 module.exports.roleList = async (req, res) => {
   const roleList = await Role.find({
     deleted: false
--- routes/admin/setting.route.js
+++ routes/admin/setting.route.js
@@ -31,6 +31,10 @@ router.get('/account-admin/create', settingController.accountAdminCreate)
 
 router.post('/account-admin/create', upload.single("avatar"), settingController.accountAdminCreatePost)
 
+router.get('/account-admin/edit/:id', settingController.accountAdminEdit)
+
+router.patch('/account-admin/edit/:id', upload.single("avatar"), settingController.accountAdminEditPatch)
+
 router.get('/role/list', settingController.roleList)
 
 router.get('/role/create', settingController.roleCreate)
