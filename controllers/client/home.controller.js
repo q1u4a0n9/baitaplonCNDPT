285143a763b29c160cfb48297c39ecf228416969
Nam Dang <namdp.28tech@gmail.com>
2025-04-01 20:50:14 +0700

    04. Hiển thị data ra Section 2 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,26 @@
-module.exports.home = (req, res) => {
+const moment = require("moment");
+const Tour = require("../../models/tour.model");
+
+module.exports.home = async (req, res) => {
+  // Section 2
+  const tourListSection2 = await Tour
+    .find({
+      priceNewAdult: { $gt: 0 }, // Lọc các tour có giá mới lớn hơn 0
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      positon: "desc"
+    })
+    .limit(6);
+  
+    for(const item of tourListSection2) {
+      item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+    }
+  // End Section 2
+
   res.render("client/pages/home", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    tourListSection2: tourListSection2
   });
 }
