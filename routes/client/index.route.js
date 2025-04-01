df613b515a5d06305f153d1c696e714de974fc3e
Nam Dang <namdp.28tech@gmail.com>
2025-04-01 19:46:00 +0700

    02. Hiển thị data ra Top Header

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,10 @@ const homeRoutes = require("./home.route");
 const tourRoutes = require("./tour.route");
 const cartRoutes = require("./cart.route");
 
+const settingMiddleware = require("../../middlewares/client/setting.middleware");
+
+router.use(settingMiddleware.websiteInfo);
+
 router.use("/", homeRoutes)
 router.use("/tours", tourRoutes)
 router.use("/cart", cartRoutes)
