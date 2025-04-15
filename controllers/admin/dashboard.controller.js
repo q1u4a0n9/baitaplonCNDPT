659090af2424eadce6000da525c547f37f9f7faf
Nam Dang <namdp.28tech@gmail.com>
2025-04-15 19:52:07 +0700

    3.1. Khối Thông số tổng quan

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,33 @@
+const Order = require("../../models/order.model");
+const AccountAdmin = require("../../models/account-admin.model");
+
 module.exports.dashboard = async (req, res) => {
+  // Thông số tổng quan
+  const overview = {
+    totalAdmin: 0,
+    totalUser: 0,
+    totalOrder: 0,
+    totalRevenue: 0
+  }
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false
+  });
+
+  // overview.totalUser = await AccountUser.countDocuments({
+  //   deleted: false
+  // });
+
+  const orderList = await Order.find({
+    deleted: false
+  });
+
+  overview.totalOrder = orderList.length;
+  overview.totalRevenue = orderList.reduce((sum, item) => sum + item.total, 0);
+  // Hết Thông số tổng quan
+
   res.render("admin/pages/dashboard", {
-    pageTitle: "Tổng quan"
+    pageTitle: "Tổng quan",
+    overview: overview
   });
 }
